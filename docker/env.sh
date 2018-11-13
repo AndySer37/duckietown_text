f33682c791728f7df6e428e045ebea8958027ae4
andyser <andy19970307@gmail.com>
2018-11-13 05:02:55 +0000

    add

--- docker/env.sh
+++ docker/env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IP=$(ifconfig wlan0 | grep "inet " | awk -F'[: ]+' '{ print $4 }')
-source /home/software/environment.sh
+source /home/duckietown_text/environment.sh
 export DUCKIEFLEET_ROOT=/data/config
 export ROS_MASTER_URI=http://${IP}:11311/
 export ROS_IP=${IP}
